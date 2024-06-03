ff899f434e8b83002dce39ef1384b6c0eb887c1a
pbaille <pierrebaille@gmail.com>
2024-06-03 20:14:43 +0200

    reapl-interop reaper-mode

--- src/elisp/reapl-interop.el
+++ src/elisp/reapl-interop.el
@@ -1,6 +1,9 @@
 ;;; reapl-interop.el -*- lexical-binding: t; -*-
 
 (require 'bencode)
+(require 'comint)
+
+;;; Code:
 
 (defun pb/thing-at-point ()
   "Return the thing at point, regardless of its type."
@@ -34,6 +37,7 @@
                            pb/reaper-socket-repl-port
                            s))
 
+
         (defun pb/send-fnl-s-expression-to-reaper-socket-repl ()
           (interactive)
           (let ((s (pb/thing-at-point)))
@@ -43,4 +47,57 @@
         '(progn :try
                 ((fn [a b] (+ a b)) 3 4)))
 
+(progn :reapl
+
+       (defvar reapl-program "/Users/pierrebaille/Code/WIP/reapl/scripts/proxy-repl.py")
+
+       (defun pb/reaper-repl ()
+         (interactive)
+         (with-current-buffer (or (get-buffer "*reapl*")
+                                  (make-comint "reapl" reapl-program))
+           (+popup/buffer)))
+
+       (defun pb/reapl-send-string (s)
+         (comint-send-string (get-buffer-process "*reapl*") (concat s "\C-d"))
+         (with-current-buffer (get-buffer "*reapl*")
+           (comint-send-eof)))
+
+       (defun pb/reapl-send-thing-at-point ()
+         (interactive)
+         (pb/reapl-send-string (pb/thing-at-point)))
+
+       (defun pb/reapl-send-buffer ()
+         (interactive)
+         (pb/reapl-send-string (buffer-substring-no-properties (point-min) (point-max)))))
+
+(progn :reaper-mode
+
+       (defvar reaper-mode-map (make-sparse-keymap))
+
+       (define-derived-mode reaper-mode
+         fennel-mode
+         "reaper mode"
+         :keymap reaper-mode-map)
+
+       (map! :localleader
+             (:map reaper-mode-map
+                   "r" #'pb/reaper-repl
+                   ))
+
+       (require 'symex-interface)
+       (defun pb/reapl-no-op ()
+         nil)
+
+       (symex-interface-add
+        'reaper-mode
+        (list :eval #'pb/reapl-send-thing-at-point
+              :eval-definition (lambda () (symex-goto-lowest) (pb/reapl-send-thing-at-point))
+              :eval-pretty #'pb/reapl-send-thing-at-point
+              :eval-thunk #'pb/reapl-no-op
+              :eval-print #'pb/reapl-no-op
+              :describe-symbol #'pb/reapl-no-op
+              :repl #'pb/reaper-repl
+              :run #'pb/reapl-send-buffer)))
+
 (provide 'reapl-interop)
+;;; reapl-interop.el ends here
