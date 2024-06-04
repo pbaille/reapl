4ab5d228b9d4b88be8ef1ae066ad3b28a5e409bb
pbaille <pierrebaille@gmail.com>
2024-06-04 09:25:10 +0200

    add noConsole option to socket repl

--- compiled/socket-repl.lua
+++ compiled/socket-repl.lua
@@ -5,64 +5,86 @@ local json = require("dkjson")
 local fnl = require("fennel")
 u = require("pb-utils")
 ru = require("reaper-utils")
-local _local_1_ = ru
-local _local_2_ = _local_1_["misc"]
-local log = _local_2_["log"]
 local r = reaper
 local udp = assert(sok.udp())
 local udp_out = assert(sok.udp())
-local function setup_udp(in_port, out_port)
+local function setup_udp(_1_)
+  local _arg_2_ = _1_
+  local socketPort = _arg_2_["socketPort"]
+  local peerPort = _arg_2_["peerPort"]
+  local noConsole = _arg_2_["noConsole"]
   udp:setoption("reuseaddr", true)
-  assert(udp:setsockname("127.0.0.1", in_port))
-  assert(udp_out:setpeername("127.0.0.1", out_port))
-  return log(("setting udp sockets:\n" .. "in: localhost:" .. in_port .. "\nout: localhost:" .. out_port .. "\n\nWelcome to fennel repl !\n"))
-end
-local function log_as_error(err)
-  return log(("error:\n\n" .. (err or "nil")))
+  assert(udp:setsockname("127.0.0.1", socketPort))
+  assert(udp_out:setpeername("127.0.0.1", peerPort))
+  if not noConsole then
+    return ru.misc.log(("setting udp sockets:\n" .. "in: localhost:" .. __fnl_global__in_2dport .. "\nout: localhost:" .. __fnl_global__out_2dport .. "\n\nWelcome to fennel repl !\n"))
+  else
+    return nil
+  end
 end
 local function send_back_as_error(e)
   return udp_out:send(json.encode({error = e}, {}))
 end
-local function repl()
-  udp:settimeout(0.0001)
-  local m = udp:receive()
-  if m then
-    local _let_3_ = bencode.decode(m)
-    local code = _let_3_["code"]
-    local compiled = _let_3_["compiled"]
-    local no_return = _let_3_["no-return"]
-    local compiled_3f, ret = nil, nil
-    local function _4_()
-      return (compiled or fnl["compile-string"](code))
+local function repl_fn(_4_)
+  local _arg_5_ = _4_
+  local noConsole = _arg_5_["noConsole"]
+  local log
+  if noConsole then
+    local function _6_(_)
     end
-    compiled_3f, ret = pcall(_4_)
-    log(("__________\n\n>> " .. code .. "\n"))
-    if not compiled_3f then
-      log_as_error(ret)
-      do local _ = (no_return or send_back_as_error(ret)) end
-    else
-      local f, err = load(ret)
-      local success, ret0 = pcall(f)
-      if success then
-        log(ret0)
-        local function _5_()
-          return udp_out:send(json.encode(ret0, {}))
-        end
-        do local _ = (no_return or xpcall(_5_, send_back_as_error)) end
-      else
-        log_as_error(ret0)
-        do local _ = (no_return or send_back_as_error(ret0)) end
-      end
-    end
+    log = _6_
   else
+    log = ru.misc.log
   end
-  return reaper.defer(repl)
+  local log_as_error
+  local function _8_(err)
+    return log(("error:\n\n" .. (err or "nil")))
+  end
+  log_as_error = _8_
+  local function repl()
+    udp:settimeout(0.0001)
+    local m = udp:receive()
+    if m then
+      local _let_9_ = bencode.decode(m)
+      local code = _let_9_["code"]
+      local compiled = _let_9_["compiled"]
+      local no_return = _let_9_["no-return"]
+      local compiled_3f, ret = nil, nil
+      local function _10_()
+        return (compiled or fnl["compile-string"](code))
+      end
+      compiled_3f, ret = pcall(_10_)
+      log(("__________\n\n>> " .. code .. "\n"))
+      if not compiled_3f then
+        log_as_error(ret)
+        do local _ = (no_return or send_back_as_error(ret)) end
+      else
+        local f, err = load(ret)
+        local success, ret0 = pcall(f)
+        if success then
+          log(ret0)
+          local function _11_()
+            return udp_out:send(json.encode(ret0, {}))
+          end
+          do local _ = (no_return or xpcall(_11_, send_back_as_error)) end
+        else
+          log_as_error(ret0)
+          do local _ = (no_return or send_back_as_error(ret0)) end
+        end
+      end
+    else
+    end
+    return reaper.defer(repl)
+  end
+  return repl
 end
-local function start_repl(_9_)
-  local _arg_10_ = _9_
-  local socketPort = _arg_10_["socketPort"]
-  local peerPort = _arg_10_["peerPort"]
-  setup_udp(socketPort, peerPort)
-  return repl()
+local function start_repl(_15_)
+  local _arg_16_ = _15_
+  local options = _arg_16_
+  local socketPort = _arg_16_["socketPort"]
+  local peerPort = _arg_16_["peerPort"]
+  local noConsole = _arg_16_["noConsole"]
+  setup_udp(options)
+  return repl_fn(options)()
 end
 return start_repl
--- src/fennel/socket-repl.fnl
+++ src/fennel/socket-repl.fnl
@@ -6,52 +6,52 @@
 (global u (require :pb-utils))
 (global ru (require :reaper-utils))
 
-(local {:misc {: log}} ru)
-
 (local r reaper)
 
 (local udp (assert (sok.udp)))
 (local udp-out (assert (sok.udp)))
 
-(fn setup-udp [in-port out-port]
+(fn setup-udp [{: socketPort : peerPort : noConsole}]
   (udp:setoption "reuseaddr" true)
-  (assert (udp:setsockname "127.0.0.1" in-port))
-  (assert (udp-out:setpeername "127.0.0.1" out-port))
-  (log (.. "setting udp sockets:\n"
-           "in: localhost:" in-port
-           "\nout: localhost:" out-port
-           "\n\nWelcome to fennel repl !\n")))
-
-(fn log-as-error [err]
-  (log (.. "error:\n\n" (or err "nil"))))
+  (assert (udp:setsockname "127.0.0.1" socketPort))
+  (assert (udp-out:setpeername "127.0.0.1" peerPort))
+  (if (not noConsole)
+      (ru.misc.log (.. "setting udp sockets:\n"
+                       "in: localhost:" in-port
+                       "\nout: localhost:" out-port
+                       "\n\nReapl server running !\n"))))
 
 (fn send-back-as-error [e]
   (udp-out:send (json.encode {:error e} {})))
 
 
-(fn repl []
-  (udp:settimeout 0.0001)
-  (local m (udp:receive))
-  (if m
-      (let [{: code : compiled : no-return} (bencode.decode m)
-            (compiled? ret) (pcall (fn [] (or compiled (fnl.compile-string code))))]
-        (log (.. "__________\n\n>> " code "\n"))
-        (if (not compiled?)
-            (do (log-as-error ret)
-                (or no-return
-                    (send-back-as-error ret)))
-            (let [(f err) (load ret)
-                  (success ret) (pcall f)]
-              (if success
-                  (do (log ret)
-                      (or no-return
-                          (xpcall (fn [] (udp-out:send (json.encode ret {})))
-                                  send-back-as-error)))
-                  (do (log-as-error ret)
-                      (or no-return
-                          (send-back-as-error ret))))))))
-  (reaper.defer repl))
+(fn repl-fn [{: noConsole}]
+  (let [log (if noConsole (fn [_]) ru.misc.log)
+        log-as-error (fn [err]
+                       (log (.. "error:\n\n" (or err "nil"))))]
+    (fn repl []
+      (udp:settimeout 0.0001)
+      (local m (udp:receive))
+      (if m
+          (let [{: code : compiled : no-return} (bencode.decode m)
+                (compiled? ret) (pcall (fn [] (or compiled (fnl.compile-string code))))]
+            (log (.. "__________\n\n>> " code "\n"))
+            (if (not compiled?)
+                (do (log-as-error ret)
+                    (or no-return
+                        (send-back-as-error ret)))
+                (let [(f err) (load ret)
+                      (success ret) (pcall f)]
+                  (if success
+                      (do (log ret)
+                          (or no-return
+                              (xpcall (fn [] (udp-out:send (json.encode ret {})))
+                                      send-back-as-error)))
+                      (do (log-as-error ret)
+                          (or no-return
+                              (send-back-as-error ret))))))))
+      (reaper.defer repl))))
 
-(fn start-repl [{: socketPort : peerPort}]
-  (setup-udp socketPort peerPort)
-  (repl))
+(fn start-repl [{&as options : socketPort : peerPort : noConsole}]
+  (setup-udp options)
+  ((repl-fn options)))
