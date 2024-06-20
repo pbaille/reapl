bba0d4926906d766b7a32d788f24ad18baa927c4
pbaille <pierrebaille@gmail.com>
2024-06-20 17:53:12 +0200

    code format

--- src/elisp/reapl-hydra.el
+++ src/elisp/reapl-hydra.el
@@ -16,23 +16,23 @@
 
 (defun reapl-hydra_init ()
   "Init the reaper hydra."
-  (reapl-mode_send-message :eval
-                           "(actions.get-binding-tree)"
-                           (lambda (res)
-                             (let* ((data (plist-get res :data))
-                                    (tree (plist-get data :value))
-                                    (children (pb-walk_pre tree
-                                                           (lambda (x)
-                                                             (if-let ((fn (plist-get x :fn)))
-                                                                 (plist-put x :fn `(lambda ()
-                                                                                     (interactive)
-                                                                                     (reapl-mode_send-action ,fn)))
-                                                               x)))))
-                               (eval (hydrata_compile
-                                      :reaper
-                                      (list :options (list :foreign-keys 'warn)
-                                            :doc "reaper root"
-                                            :children children)))))))
+  (reapl-mode_send-message
+   :eval "(actions.get-binding-tree)"
+   (lambda (res)
+     (let* ((data (plist-get res :data))
+            (tree (plist-get data :value))
+            (children (pb-walk_pre tree
+                                   (lambda (x)
+                                     (if-let ((fn (plist-get x :fn)))
+                                         (plist-put x :fn `(lambda ()
+                                                             (interactive)
+                                                             (reapl-mode_send-action ,fn)))
+                                       x)))))
+       (eval (hydrata_compile
+              :reaper
+              (list :options (list :foreign-keys 'warn)
+                    :doc "reaper root"
+                    :children children)))))))
 
 (provide 'reapl-hydra)
 ;;; reapl-hydra.el ends here
